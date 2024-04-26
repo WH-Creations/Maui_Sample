7c0edceab67d4f086658e2e4dae26c179b4ab4dd
WH-Creations <casey@westhousecreations.com>
2024-04-25 18:41:52 -0700

    Base ViewModel & Base View
    - Setup base view model with IAsyncRelayCommand to handle loading initial data. Also add base content page that triggers the interfaces initialize command.

--- Casey_West_Maui_Sample/Maui_App/ViewModels/IBaseViewModel.cs
+++ Casey_West_Maui_Sample/Maui_App/ViewModels/IBaseViewModel.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.Input;
 
 namespace Maui_App.ViewModels
 {
-    internal class IBaseViewModel
+    internal interface IBaseViewModel
     {
+        // Command to initialize the ViewModel asynchronously.
+        IAsyncRelayCommand InitializeAsyncCommand { get; }
     }
 }
---
+++ Casey_West_Maui_Sample/Maui_App/Views/BaseContentPage.cs
@@ -0,0 +1,39 @@
+﻿using Maui_App.ViewModels;
+using System;
+using Microsoft.Maui.Controls;
+
+namespace Maui_App.Views
+{
+    internal class BaseContentPage : ContentPage
+    {
+        private bool _isInitialized = false;
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Check if context conforms to base VM interface & hasn't been initialized
+            if (_isInitialized || BindingContext is not IBaseViewModel viewModel)
+            {
+                return;
+            }
+
+            try
+            {
+                await viewModel.InitializeAsyncCommand.ExecuteAsync(null);
+
+                // Mark as initialized to prevent re-initialization
+                _isInitialized = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Initialization failed: {ex.Message}");
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+        }
+    }
+}
